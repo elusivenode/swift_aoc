15da4436d1d349aa5f2f603ace44539ef6f2995c
Hamish MacDonald <hamish.macdonald@neds.com>
2024-12-01 10:59:52 +1000

    2015 d1 p2

--- Sources/Solutions/2015/Day1.swift
+++ Sources/Solutions/2015/Day1.swift
@@ -6,7 +6,17 @@ struct Day1: AdventSolution {
     }
     
     static func solvePart2(input: String) -> Any {
-        // Implement part 2 here
-        return 0
+        var floor = 0
+        for (index, char) in input.enumerated() {
+            if char == "(" {
+                floor += 1
+            } else {
+                floor -= 1
+            }
+            if floor == -1 {
+                return index + 1
+            }
+        }
+        return floor
     }
 }
